fb9633c61e5bcfaa1fae3460d905f8b1d5ba2d42
Денис Гура <denis777gura@gmail.com>
2019-05-28 13:50:32 +0300

    Добавьте файлы проекта.

---
+++ labb-6-2/BookInLibrary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace prog_4s_l6_2
+{
+    public class BookInLibrary : IComparable
+    {
+        public string pib;
+        public string nazva;
+        public int code;
+        private int year;
+        public int pages;
+        private string genre;
+
+
+
+        public BookInLibrary(string nazva, int year, int pages)
+        {
+            pib = "Джордж Мартин";
+            this.nazva = nazva;
+            code = 1783;
+            this.year = year;
+            this.pages = pages;
+            genre = "Фэнтези";
+        }
+        public BookInLibrary(string pib, string nazva, int code, int year, int pages, string genre)
+        {
+            this.pib = pib;
+            this.nazva = nazva;
+            this.code = code;
+            this.year = year;
+            this.pages = pages;
+            this.genre = genre;
+        }
+
+        public BookInLibrary() { }
+        public int CompareTo(object o)
+        {
+            BookInLibrary one = (BookInLibrary)o;
+            return year.CompareTo(one.year);
+        }
+        public void getInfo(int i)
+        {
+            Console.WriteLine($"Введите информацию о книге № {i}");
+            Console.Write("Введите название: ");
+            nazva = Console.ReadLine();
+            Console.Write("Введите фио автора: ");
+            pib = Console.ReadLine();
+            Console.Write("Введите код: ");
+            code = int.Parse(Console.ReadLine());
+            Console.Write("Введите год: ");
+            year = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите kol-vo str: ");
+            pages = int.Parse(Console.ReadLine());
+            Console.Write("Введите жанр: ");
+            genre = Console.ReadLine();
+
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("Книга: {0}, автор {1}, код {2}, год {3}, сожержится страниц {4}, жанр {5}", nazva, pib, code, year, pages, genre);
+            return "Книга: " + nazva + " автор " + pib + " код " + code + " год " + year + " сожержится страниц " + pages + " жанр " + genre;
+        }
+
+    }
+}
---
+++ labb-6-2/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_4s_l6_2
+{
+    class Program
+    {
+        static public void Search(BookInLibrary[] bookinlibrary)
+        {
+            try
+            {
+
+                int codeS;
+                bool flag = false;
+                Console.WriteLine("\n\nВведите код книги для поиска");
+                codeS = int.Parse(Console.ReadLine());
+                Console.WriteLine("Результат поиска:\n");
+                for (int i = 0; i < bookinlibrary.Length; i++)
+                {
+                    if (codeS == bookinlibrary[i].code)
+                    {
+                        bookinlibrary[i].ToString();
+                        flag = true;
+                    }
+                }
+                if (flag == false)
+                {
+                    Console.WriteLine("Такой книги нет в списке");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("!!! Неверный формат ввода кода книги !!!");
+            }
+        }
+
+        static public void CompareAuthor(BookInLibrary[] bookinlibrary)
+        {
+            int i1, i2;
+        again1:
+            Console.WriteLine($"Введите номер 1-й книги для сравнения(от 0 до {bookinlibrary.Length - 1})");
+            i1 = int.Parse(Console.ReadLine());
+            if (i1 > bookinlibrary.Length || i1 < 0)
+            {
+                goto again1;
+            }
+        again2:
+            Console.WriteLine($"Введите номер 2-й книги для сравнения(от 0 до {bookinlibrary.Length - 1})");
+            i2 = int.Parse(Console.ReadLine());
+            if (i2 > bookinlibrary.Length || i2 < 0)
+            {
+                goto again2;
+            }
+            if (bookinlibrary[i1].pib == bookinlibrary[i2].pib)
+            {
+                Console.WriteLine($"Книги {bookinlibrary[i1].nazva} и {bookinlibrary[i2].nazva} имеют одного автора");
+            }
+            else
+            {
+                Console.WriteLine($"Книги {bookinlibrary[i1].nazva} и {bookinlibrary[i2].nazva} имеют разных авторов");
+            }
+        }
+
+
+        static public void ComparePages(BookInLibrary[] bookinlibrary)
+        {
+            int i1, i2;
+        again1:
+            Console.WriteLine($"Введите номер 1-й книги для сравнения(от 0 до {bookinlibrary.Length - 1})");
+            i1 = int.Parse(Console.ReadLine());
+            if (i1 > bookinlibrary.Length || i1 < 0)
+            {
+                goto again1;
+            }
+        again2:
+            Console.WriteLine($"Введите номер 2-й книги для сравнения(от 0 до {bookinlibrary.Length - 1})");
+            i2 = int.Parse(Console.ReadLine());
+            if (i2 > bookinlibrary.Length || i2 < 0)
+            {
+                goto again2;
+            }
+            if (bookinlibrary[i1].pages == bookinlibrary[i2].pages)
+            {
+                Console.WriteLine($"Книги {bookinlibrary[i1].nazva} и {bookinlibrary[i2].nazva} имеют одинаковое количество страниц");
+            }
+            else
+            {
+                if (bookinlibrary[i1].pages > bookinlibrary[i2].pages)
+                {
+                    Console.WriteLine($"В книге {bookinlibrary[i1].nazva} страниц больше чем в книге {bookinlibrary[i2].nazva} на {bookinlibrary[i1].pages - bookinlibrary[i2].pages}");
+                }
+                else
+                {
+                    Console.WriteLine($"В книге {bookinlibrary[i2].nazva} страниц больше чем в книге {bookinlibrary[i1].nazva} на {bookinlibrary[i2].pages - bookinlibrary[i1].pages}");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                BookInLibrary x1 = new BookInLibrary("Игра престолов", 1996, 569);
+                BookInLibrary x2 = new BookInLibrary("Битва королей", 1998, 600);
+                BookInLibrary x3 = new BookInLibrary("Буря мечей", 2000, 541);
+                BookInLibrary x4 = new BookInLibrary("Лисенко", "Методические указания", 9485, 2019, 10, "информация");
+                BookInLibrary x5 = new BookInLibrary("Даниель Дефо", "Робинзон Крузо", 9343, 1985, 300, "приключенческий роман");
+                BookInLibrary x6 = new BookInLibrary();
+
+                BookInLibrary[] books = new BookInLibrary[] { x1, x2, x3, x4, x5, x6 };
+
+                books[5].getInfo(5);
+                for (int i = 0; i < books.Length; i++)
+                {
+                    books[i].ToString();
+                }
+
+                Search(books);
+                Console.WriteLine("\n\n");
+                CompareAuthor(books);
+                Console.WriteLine("\n\n");
+                ComparePages(books);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Список книг пуст");
+            }
+        }
+    }
+}
